a3c2fdcc8a709069a26330702041b9ece2fde0e3
unknown <subhamparidamain@gmail.com>
2023-09-23 16:00:20 +0530

    home & login, signup page done

---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Main.css'
+import Slider from '../components/Slider'
+
+const Home = () => {
+  return (
+    <>
+    {/* <div>Home</div> */}
+    <Slider></Slider>
+    </>
+  )
+}
+
+export default Home
